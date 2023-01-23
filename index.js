f5426cb33c44de338146b53ec55faa0be61ae7f0
drake bishop <drakemx85@gmail.com>
2023-01-23 01:21:05 -0600

    commit tree 0993102f50f2626fd9d027fde88e6e6d90f9326f

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+const newArray = [...cats, "Broom"];
+const newCatArray = ["Arnold", ...cats];
+const newCatsArray = cats.slice("Milo", "Otis")
+const newCatArrays = cats.slice("Otis", "Garfield")
+
+function myArray() { 
+    
+}
+
+function destructivelyAppendCat(Ralph) {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(Bob) {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(Garfield) {
+    cats.pop("Garfield")
+}
+
+function destructivelyRemoveFirstCat(Milo) {
+    cats.shift("Milo")
+}
+
+function appendCat(Broom) {
+    return [...cats, "Broom"]
+}
+
+function prependCat(Arnold) {
+    return ["Arnold", ...cats];
+}
+
+function removeLastCat(Garfield) {
+    return ["Milo", "Otis"]
+}
+
+function removeFirstCat(Milo) {
+    return ["Otis", "Garfield"]
+}
